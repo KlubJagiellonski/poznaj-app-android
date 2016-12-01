db81af9e782beb028bba357fe385ca7d9a597454
Rafał Gawlik <rafal.gawlik@gmail.com>
2016-12-01 20:51:47 +0100

    Map improvment, card view improvment, icons

--- app/src/main/java/pl/poznajapp/view/activity/AllPointActivity.java
+++ app/src/main/java/pl/poznajapp/view/activity/AllPointActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import butterknife.BindString;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import pl.poznajapp.R;
 
@@ -13,11 +15,23 @@ import pl.poznajapp.R;
 
 public class AllPointActivity  extends AppCompatActivity {
 
+    @BindView(R.id.allpoint_toolbar) android.support.v7.widget.Toolbar toolbar;
+    @BindString(R.string.apppoint__title) String toolbarTitle;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_point);
         ButterKnife.bind(this);
+
+        initToolbar();
+    }
+
+    void initToolbar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(toolbarTitle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 
     @Override
--- app/src/main/java/pl/poznajapp/view/activity/TripActivity.java
+++ app/src/main/java/pl/poznajapp/view/activity/TripActivity.java
@@ -1,5 +1,6 @@
 package pl.poznajapp.view.activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -8,28 +9,41 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import pl.poznajapp.R;
 import pl.poznajapp.model.Picture;
-import pl.poznajapp.model.Trip;
 import pl.poznajapp.view.adapter.PictureAdapter;
-import pl.poznajapp.view.adapter.TripAdapter;
 
 /**
  * Created by rafal on 29.11.2016.
  */
 
-public class TripActivity extends AppCompatActivity {
+public class TripActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @BindView(R.id.trip_toolbar) Toolbar toolbar;
     @BindView(R.id.trip_pictures) RecyclerView picturesRecyclerView;
 
+    @BindString(R.string.trip_toolbar_title) String toolbarTitle;
+
+    MapFragment supportMapFragment;
     PictureAdapter mAdapter;
     List pictures;
+    List<MarkerOptions> markers;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,18 +51,20 @@ public class TripActivity extends AppCompatActivity {
         setContentView(R.layout.activity_trip);
         ButterKnife.bind(this);
 
+        mockData();
+        initToolbar();
+        initList();
+        initMap();
+    }
+
+    void initToolbar() {
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Rzeszów");
+        getSupportActionBar().setTitle(toolbarTitle);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-
-
-        initList();
     }
 
     void initList(){
-        //TODO implement
-        mockList();
         mAdapter = new PictureAdapter(pictures);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         picturesRecyclerView.setLayoutManager(mLayoutManager);
@@ -56,7 +72,12 @@ public class TripActivity extends AppCompatActivity {
         picturesRecyclerView.setAdapter(mAdapter);
     }
 
-    void mockList(){
+    void initMap(){
+        supportMapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.trip_map);
+        supportMapFragment.getMapAsync(this);
+    }
+
+    void mockData(){
         pictures = new ArrayList<Picture>();
         Picture pic = new Picture();
         pic.setUrl("http://google.pl");
@@ -78,6 +99,51 @@ public class TripActivity extends AppCompatActivity {
         pic.setUrl("http://google.pl");
         pictures.add(pic);
 
+        LatLng PERTH = new LatLng(-31.952854, 115.857342);
+        LatLng SYDNEY = new LatLng(-33.87365, 151.20689);
+        LatLng BRISBANE = new LatLng(-27.47093, 153.0235);
+
+
+        markers = new ArrayList<>();
+        markers.add(new MarkerOptions()
+                .position(PERTH)
+                .title("Perth"));
+        markers.add(new MarkerOptions()
+                .position(SYDNEY)
+                .title("Sydnay"));
+        markers.add(new MarkerOptions()
+                .position(BRISBANE)
+                .title("Brisbane"));
+
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        googleMap.getUiSettings().setAllGesturesEnabled(false);
+        googleMap.getUiSettings().setMapToolbarEnabled(false);
+        googleMap.getUiSettings().setRotateGesturesEnabled(false);
+        googleMap.getUiSettings().setCompassEnabled(false);
+        googleMap.getUiSettings().setMyLocationButtonEnabled(false);
+        googleMap.getUiSettings().setTiltGesturesEnabled(false);
+        googleMap.getUiSettings().setZoomControlsEnabled(false);
+        googleMap.getUiSettings().setZoomGesturesEnabled(false);
+
+
+        PolylineOptions line= new PolylineOptions();
+        line.width(5).color(Color.BLACK);
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        for (MarkerOptions marker : markers) {
+            googleMap.addMarker(marker);
+            builder.include(marker.getPosition());
+            line.add(marker.getPosition());
+        }
+        LatLngBounds bounds = builder.build();
+        googleMap.addPolyline(line);
+        //TODO implement
+//        int padding = 50; // offset from edges of the map in pixels
+//        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+//        googleMap.moveCamera(cu);
+
+    }
 }
