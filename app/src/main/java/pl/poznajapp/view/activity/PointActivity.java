61ab3c5b3b57335ee813b4d0751d73937e6f4a28
Rafal Gawlik <rafal.gawlik@gmail.com>
2017-03-18 00:24:55 +0100

    PointActivity

--- app/src/main/java/pl/poznajapp/view/activity/PointActivity.java
+++ app/src/main/java/pl/poznajapp/view/activity/PointActivity.java
@@ -3,9 +3,12 @@ package pl.poznajapp.view.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import java.util.List;
 
+import butterknife.BindView;
 import pl.poznajapp.R;
 
 /**
@@ -17,6 +20,10 @@ public class PointActivity extends AppCompatActivity {
     public static final String POINT_TITLE = "POINT_TITLE";
     public static final String POINT_DESCRIPTION = "POINT_DESCRIPTION";
     public static final String POINT_IMAGES = "POINT_IMAGES";
+
+    @BindView(R.id.point_toolbar) Toolbar toolbar;
+    @BindView(R.id.point_description) TextView pointDescription;
+
 
     String title;
     String description;
@@ -35,6 +42,9 @@ public class PointActivity extends AppCompatActivity {
         title = getIntent().getStringExtra(POINT_TITLE);
         description = getIntent().getStringExtra(POINT_DESCRIPTION);
         images = getIntent().getStringArrayListExtra(POINT_IMAGES);
+
+        toolbar.setTitle(title);
+        pointDescription.setText(description);
     }
 
     @Override
