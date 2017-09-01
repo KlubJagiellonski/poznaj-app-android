dcd0d57b73cfdb636250bb750e7edc9396088fa5
Rafal Gawlik <rafal.gawlik@gmail.com>
2017-09-01 22:52:14 +0200

    Database

--- app/src/main/java/pl/poznajapp/database/Database.java
+++ app/src/main/java/pl/poznajapp/database/Database.java
@@ -1,9 +1,26 @@
 package pl.poznajapp.database;
 
+import java.util.List;
+
+import io.realm.Realm;
+import pl.poznajapp.database.model.PointObject;
+import pl.poznajapp.database.model.StoryObject;
+
 /**
  * Created by Rafał Gawlik on 23.08.17.
  */
 
 public class Database {
+
+    public void saveStory(Integer id, String title, String description){
+        Realm.deleteRealm(Realm.getDefaultConfiguration());
+        Realm realm = Realm.getDefaultInstance();
+        realm.beginTransaction();
+        StoryObject story = realm.createObject(StoryObject.class);
+        story.setId(id);
+        story.setTitle(title);
+        story.setDescription(description);
+        realm.commitTransaction();
+    }
 
 }
---
+++ app/src/main/java/pl/poznajapp/database/model/StoryObject.java
@@ -0,0 +1,38 @@
+package pl.poznajapp.database.model;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by Rafał Gawlik on 01.09.17.
+ */
+
+public class StoryObject extends RealmObject {
+
+    private Integer id;
+    private String title;
+    private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
