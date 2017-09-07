7e0bfe67080fa919e41687ada9885286e2dd8908
Mikołaj Rod <Salezjana@users.noreply.github.com>
2017-09-07 23:59:42 +0200

    add menu

--- app/src/main/java/pl/poznajapp/view/MainActivity.java
+++ app/src/main/java/pl/poznajapp/view/MainActivity.java
@@ -24,7 +24,10 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -95,6 +98,29 @@ public class MainActivity extends BaseAppCompatActivity {
             requestPermissions();
         } else {
             checkLocationEnabled();
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle item selection
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.action_one:
+
+                return true;
+            case R.id.action_two:
+
+                return true;
+            case R.id.action_three:
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 
